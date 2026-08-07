3dfb9601f8f6b5e73a626acb755ca5df7ced09f9
Kyle <kyle201817146@gmail.com>
2026-08-07 14:55:59 +0800

    chore: prepare 0.3.0 release

--- Project.swift
+++ Project.swift
@@ -28,7 +28,7 @@ let project = Project(
                 "CFBundleInfoDictionaryVersion": "6.0",
                 "CFBundleName": "$(PRODUCT_NAME)",
                 "CFBundlePackageType": "APPL",
-                "CFBundleShortVersionString": "0.2.1",
+                "CFBundleShortVersionString": "0.3.0",
                 "CFBundleURLTypes": [
                     [
                         "CFBundleTypeRole": "Editor",
@@ -36,7 +36,7 @@ let project = Project(
                         "CFBundleURLSchemes": ["swifty-download-manager"],
                     ],
                 ],
-                "CFBundleVersion": "3",
+                "CFBundleVersion": "4",
                 "LSApplicationCategoryType": "public.app-category.productivity",
                 "LSSupportsOpeningDocumentsInPlace": true,
                 "NSAppTransportSecurity": [
@@ -107,8 +107,8 @@ let project = Project(
                 "CFBundleInfoDictionaryVersion": "6.0",
                 "CFBundleName": "$(PRODUCT_NAME)",
                 "CFBundlePackageType": "XPC!",
-                "CFBundleShortVersionString": "0.2.1",
-                "CFBundleVersion": "3",
+                "CFBundleShortVersionString": "0.3.0",
+                "CFBundleVersion": "4",
                 "NSExtension": [
                     "NSExtensionPointIdentifier": "com.apple.Safari.web-extension",
                     "NSExtensionPrincipalClass": "$(PRODUCT_MODULE_NAME).SafariWebExtensionHandler",
